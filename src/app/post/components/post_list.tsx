3b717146402cb0cb9f86dfef015e2a1f817c601e
wolfather <so.israelweb@gmail.com>
2025-10-12 06:54:09 -0300

    enhance html

--- src/app/post/components/post_list.tsx
+++ src/app/post/components/post_list.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { db } from "@/app/services/client/firebaseconfig"
-import { collection, onSnapshot, query } from "firebase/firestore"
+import { collection, onSnapshot, query, orderBy, limit, where, } from "firebase/firestore"
 import { useEffect, useState } from "react"
 
 interface Posts {
@@ -10,31 +10,34 @@ export function PostList({ posts }: Posts) {
     const [list, setList] = useState(posts)
 
     useEffect(() => {
-        const q = query(collection(db, 'posts'))
-        const unsubs = onSnapshot(q, (snap) => {
-            const updatePosts = snap.docs.map(doc => {
-                return {
-                    id: doc.id,
-                    text: doc.data().text,
-                    created_at: doc.data().created_at.seconds,
-                    public: doc.data().public,
-                }
-            });
+        const q = query(
+            collection(db, 'posts'), 
+            orderBy('created_at', 'desc'),
+            limit(5),
+        );
 
-            setList(updatePosts)
-        })
+        const unsubs = onSnapshot(q, (snap) => {
+            const updatePosts = snap.docs.map(doc => ({
+                id: doc.id,
+                text: doc.data().text,
+                created_at: doc.data().created_at.seconds,
+                public: doc.data().public,
+            }));
+
+            setList(updatePosts);
+        });
 
         return () => unsubs();
     }, []);
 
     return (
-        <div>
+        <section>
             {list.map((item: any) => (
-                <div>
+                <div className="flex justify-between">
                     {item.public && <span>pública</span>}
                     <p key={item.id}>{item.text}</p>
                 </div>
             ))}
-        </div>
+        </section>
     )
 }
